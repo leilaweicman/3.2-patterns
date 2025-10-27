70687778f5c6e35acf7bbd9ec920ef22933c42a4
leilaweicman <leilaweic@hotmail.com>
2025-10-27 12:56:14 +0100

    feat: create Main class to demonstrate pizza creation using classic Builder pattern with MestrePizzer

---
+++ src/main/java/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+
+        PizzaBuilder hawaiianBuilder = new HawaiianPizzaBuilder();
+        MestrePizzer mestre = new MestrePizzer(hawaiianBuilder);
+        mestre.constructPizza();
+        Pizza hawaiianPizza = mestre.getPizza();
+        System.out.println("First pizza built: " + hawaiianPizza);
+
+        PizzaBuilder vegetarianBuilder = new VegetarianPizzaBuilder();
+        mestre = new MestrePizzer(vegetarianBuilder);
+        mestre.constructPizza();
+        Pizza vegetarianPizza = mestre.getPizza();
+        System.out.println("Second pizza built: " + vegetarianPizza);
+    }
+}
