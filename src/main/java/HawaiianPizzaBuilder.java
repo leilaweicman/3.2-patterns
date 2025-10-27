e3fe325915a90a9d0083f31753d4f0ab7add7b32
leilaweicman <leilaweic@hotmail.com>
2025-10-27 12:23:51 +0100

    feat: implement HawaiianPizzaBuilder with default configuration and build steps

--- src/main/java/HawaiianPizzaBuilder.java
+++ src/main/java/HawaiianPizzaBuilder.java
@@ -1,2 +1,38 @@
-public class HawaiianPizzaBuilder {
+import java.util.Arrays;
+import java.util.List;
+
+public class HawaiianPizzaBuilder implements PizzaBuilder {
+
+    private final Pizza pizza;
+
+    public HawaiianPizzaBuilder() {
+        this.pizza = new Pizza();
+    }
+
+    @Override
+    public void setSize(String size) {
+        pizza.setSize(size);
+    }
+
+    @Override
+    public void setDough(String dough) {
+        pizza.setDough(dough);
+    }
+
+    @Override
+    public void setToppings(List<String> toppings) {
+        pizza.setToppings(toppings);
+    }
+
+    @Override
+    public Pizza build() {
+        return pizza;
+    }
+
+    public Pizza buildDefault() {
+        setSize("Large");
+        setDough("Thick");
+        setToppings(Arrays.asList("Ham", "Pineapple", "Cheese"));
+        return build();
+    }
 }
