3f9600a10c01a8ffb0b3e8e984e10b40d281ceff
leilaweicman <leilaweic@hotmail.com>
2025-10-28 12:27:40 +0100

    feat: implement PaymentGateway to execute payments and trigger callback notification

---
+++ src/main/java/nivel3/gateway/PaymentGateway.java
@@ -0,0 +1,18 @@
+package nivel3.gateway;
+
+import nivel3.payments.PaymentMethod;
+
+public class PaymentGateway {
+    public void executePayment(PaymentMethod method, double amount, PaymentCallback callback) {
+        System.out.println("\nStarting payment process...");
+        method.processPayment(amount);
+
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        callback.onPaymentComplete("✅ Payment of $" + amount + " processed successfully.");
+    }
+}
